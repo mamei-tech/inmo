d2aad19ecb012296a97525ce3ebc4d8bd6f21d95
Eduardo <elbranad@gmail.com>
2018-09-08 21:55:05 -0400

    terminada vista de contact me. falta la parte dinamica

--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -7,16 +7,6 @@ return [
     'guides' => 'Guias',
     'aboutMe' => 'Acerca de mi',
     'contact' => 'Contacto',
-    'contactMe' => 'Contáctame',
-    'moreInformation' => 'Obtener más información',
-    'yourName' => 'tu nombre',
-    'yourEmail' => 'tu correo',
-    'yourPhone' => 'tu teléfono',
-    'writeMe' => 'escríbeme',
-    'send' => 'enviar',
-    'testimonials' => 'testimonials', //TODO Buscar traduccion pa esto
-    'expressOpinion' => 'tu puedes expresar tu opinión acerca de mis servicios',
-    'yourtestimonials' => 'tu testimonials', //TODO Buscar traduccion pa esto
     'slide1-h1-1' => 'SU NUEVO HOGAR',
     'slide1-h1-2' => 'ESTÁ A UNA LLAMADA DE DISTANCIA',
     'slide1-h2' => '¡llámame hoy!',
@@ -25,5 +15,15 @@ return [
     'slide2-h2' => 'Pagando los mejores precios',
     'slide3-h1-1' => 'DISFRUTA DE LAS MEJORES VISTAS',
     'slide3-h1-2' => 'DE UN VERANO',
-    'slide3-h2' => 'que dura todo el año'
+    'slide3-h2' => 'que dura todo el año',
+    'contactMe' => 'Contáctame',
+    'moreInformation' => 'Obtener más información',
+    'yourName' => 'tu nombre',
+    'yourEmail' => 'tu correo',
+    'yourPhone' => 'tu teléfono',
+    'writeMe' => 'escríbeme',
+    'send' => 'enviar',
+    'testimonials' => 'testimonials', //TODO Buscar traduccion pa esto
+    'expressOpinion' => 'puedes expresar tu opinión acerca de mis servicios',
+    'yourtestimonials' => 'tu testimonials', //TODO Buscar traduccion pa esto
 ];
