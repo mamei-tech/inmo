d4446c016b4cbfffbf9279ebbfffe57927938669
Eduardo <Eduardo@DESKTOP-KH0U7DU>
2019-01-24 11:44:55 -0500

    cambios que traje de ksa de pozo

--- resources/views/houses.blade.php
+++ resources/views/houses.blade.php
@@ -8,235 +8,264 @@
 @section('content')
 
 
-
-<style type="text/css">
-	.IDX-wrapper-standard #IDX-resultsRefineSearchWrap,
-	.IDX-wrapper-standard .IDX-resultsPhotogallery,
-    .IDX-wrapper-standard .IDX-resultsVirtualTour,
-    .IDX-wrapper-standard .IDX-resultsSaveProperty,
-    .IDX-wrapper-standard .IDX-resultsDetailsLink,
-    #IDX-resultsSeparatorActive,.IDX-resultsSeparatorText,
-    #IDX-resultsHeader #IDX-resultsPager-header,
-    #IDX-resultsHeader #IDX-resultsCountWrap,
-    #IDX-resultsHeader #IDX-resultsCountMessage,
-    #IDX-resultsHeader #IDX-resultsTopActions,
-    #IDX-resultsHeader #IDX-resultsRefineSearchToggle,
-    #IDX-resultsHeader #IDX-resultsRefineSearchWrap,
-    #IDX-resultsFooter,
-    .IDX-wrapper-standard #IDX-resultsMLSRights,
-    .IDX-wrapper-standard .IDX-resultsDescription {
-		display: none;
-	}
-	.IDX-resultsCellsContainer{
-		display: flex;
-		flex-wrap: wrap;
-	}
-
-	.IDX-wrapper-standard .IDX-resultsPhoto, .IDX-wrapper-standard .IDX-resultsAddress {
-        width: 100%;
-        float: none;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsMainInfo.IDX-panel-default, .IDX-wrapper-standard .IDX-resultsAddress {
-        padding-left: 0;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsPhoto{
-    	height: 220px;
-    	overflow: hidden;
-    }
-
-    .IDX-cellInnerWrapper, .IDX-cellInnerWrapper > .IDX-row-content{
-        height: 100%;
-    }
-
-    .IDX-cellInnerWrapper > .IDX-row-content{
-        display: flex;
-        flex-direction: column;
-    }
-
-    #IDX-main{
-        background-color: rgb(228, 228, 228);
-        padding: 0;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsCell{
-        border-bottom: none;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsAddress{
-        font-size: 12px;
-        font-family: 'SinkinSans-600SemiBold', sans-serif;
-        letter-spacing: 2px;
-        text-transform: uppercase;
-        line-height: 20px;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsAddress a{
-        color: rgb(142, 142, 142);
-    }
-
-    .IDX-wrapper-standard .IDX-resultsAddress a:hover{
-        text-decoration: none;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsMainInfo.IDX-panel-default{
-        float: none;
-        width: 100%;
-        font-size: 12px;
-        font-family: 'SinkinSans-500Medium', sans-serif;
-        line-height: 20px;
-        color: rgb(142, 142, 142);
-
-        flex-direction: column;
-        flex: 1;
-        justify-content: flex-end;
-
-        display: none;
-    }
-
-    #IDX-main > div:last-child{
-        display: none !important;
-    }
-
-    .section-0{
-        padding: 50px 120px;
-        background-color: rgb(228, 228, 228);
-        margin-top: -100px;
-        padding-top: 150px;
-    }
-
-
-    .IDX-wrapper-standard .IDX-resultsCell{
-        flex: 0 0 25%;
-        max-width: 25%;
-        padding: 0 15px;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsCell:nth-child(4n+1){
-        padding-left: 0;
-        margin-left: 0;
-    }
-
-    .IDX-wrapper-standard .IDX-resultsCell:nth-child(4n){
-        padding-right: 0;
-        margin-right: 0;
-    }
-
-    .IDX-resultsMainInfo{
-        display: none;
-    }
-
-
-    @media (max-width: 1249px) {
-        .section-0 {
-            padding: 50px 100px;
-            margin-top: -80px;
-            padding-top: 130px;
+    <style type="text/css">
+        .IDX-wrapper-standard #IDX-resultsRefineSearchWrap,
+        .IDX-wrapper-standard .IDX-resultsPhotogallery,
+        .IDX-wrapper-standard .IDX-resultsVirtualTour,
+        .IDX-wrapper-standard .IDX-resultsSaveProperty,
+        .IDX-wrapper-standard .IDX-resultsDetailsLink,
+        #IDX-resultsSeparatorActive,.IDX-resultsSeparatorText,
+        #IDX-resultsHeader #IDX-resultsPager-header,
+        #IDX-resultsHeader #IDX-resultsCountWrap,
+        #IDX-resultsHeader #IDX-resultsCountMessage,
+        #IDX-resultsHeader #IDX-resultsTopActions,
+        #IDX-resultsHeader #IDX-resultsRefineSearchToggle,
+        #IDX-resultsHeader #IDX-resultsRefineSearchWrap,
+        #IDX-resultsFooter,
+        .IDX-wrapper-standard #IDX-resultsMLSRights,
+        .IDX-wrapper-standard .IDX-resultsDescription {
+            display: none;
+        }
+        .IDX-resultsCellsContainer{
+            display: flex;
+            flex-wrap: wrap;
         }
 
-        .IDX-wrapper-standard .IDX-resultsCell{
-            flex: 0 0 50%;
-            max-width: 50%;
-            padding: 0 20px;
+        .IDX-wrapper-standard .IDX-resultsPhoto, .IDX-wrapper-standard .IDX-resultsAddress {
+            width: 100%;
+            float: none;
         }
 
-        .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n+1){
+        .IDX-wrapper-standard .IDX-resultsMainInfo.IDX-panel-default, .IDX-wrapper-standard .IDX-resultsAddress {
             padding-left: 0;
-            margin-left: 0;
         }
 
-        .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n){
-            padding-right: 0;
-            margin-right: 0;
+        .IDX-wrapper-standard .IDX-resultsPhoto{
+            height: 220px;
+            overflow: hidden;
         }
 
-        .IDX-wrapper-standard .IDX-resultsPhoto {
-            text-align: initial;
-        }
-    }
-
-    @media (max-width: 960px) {
-        .section-0 {
-            padding: 40px 30px;
-            margin-top: -80px;
-            padding-top: 120px;
+        .IDX-cellInnerWrapper, .IDX-cellInnerWrapper > .IDX-row-content{
+            height: 100%;
         }
 
-        .IDX-wrapper-standard .IDX-resultsCell{
-            flex: 0 0 100%;
-            max-width: 100%;
+        .IDX-cellInnerWrapper > .IDX-row-content{
+            display: flex;
+            flex-direction: column;
+        }
+
+        #IDX-main{
+            background-color: rgb(228, 228, 228);
             padding: 0;
         }
 
-        .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n+1){
+        .IDX-wrapper-standard .IDX-resultsCell{
+            border-bottom: none;
+        }
+
+        .IDX-wrapper-standard .IDX-resultsPhotoImg {
+            height: 100%;
+            width: 100%;
+        }
+
+        .IDX-wrapper-standard .IDX-resultsAddress{
+            text-transform: uppercase;
+        }
+
+        .IDX-wrapper-standard .IDX-resultsAddress a{
+            color: rgb(142, 142, 142);
+        }
+
+        .IDX-wrapper-standard .IDX-resultsAddress a:hover{
+            text-decoration: none;
+        }
+
+        .IDX-wrapper-standard .IDX-resultsMainInfo.IDX-panel-default{
+            float: none;
+            width: 100%;
+            color: rgb(142, 142, 142);
+
+            flex-direction: column;
+            flex: 1;
+            justify-content: flex-end;
+
+            display: none;
+        }
+
+        .IDX-wrapper-standard .IDX-resultsMainInfo.IDX-panel-default > div:last-child{
+            font-family: 'SinkinSans-700Bold';
+        }
+
+        #IDX-main > div:last-child{
+            display: none !important;
+        }
+
+        .section-0{
+            padding: 50px 120px;
+            background-color: rgb(228, 228, 228);
+            margin-top: -100px;
+            padding-top: 150px;
+        }
+
+
+        .IDX-wrapper-standard .IDX-resultsCell{
+            flex: 0 0 25%;
+            max-width: 25%;
+            padding: 0 15px;
+        }
+
+        .IDX-wrapper-standard .IDX-resultsCell:nth-child(4n+1){
             padding-left: 0;
             margin-left: 0;
         }
 
-        .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n){
+        .IDX-wrapper-standard .IDX-resultsCell:nth-child(4n){
             padding-right: 0;
             margin-right: 0;
         }
 
-        .IDX-wrapper-standard .IDX-resultsPhoto {
-            text-align: initial;
+        .IDX-resultsMainInfo{
+            display: none;
         }
-    }
 
 
-    /*Pagination*/
-    .container-pagination{
-        display: flex;
-        margin-bottom: 30px;
-    }
+        @media (max-width: 1249px) {
+            .section-0 {
+                padding: 50px 100px;
+                margin-top: -80px;
+                padding-top: 130px;
+            }
 
-    .pagination {
-        display: block;
-    }
+            .IDX-wrapper-standard .IDX-resultsCell{
+                flex: 0 0 50%;
+                max-width: 50%;
+                padding: 0 20px;
+                margin-bottom: 45px;
+            }
 
-    .pagination{
-        min-height: 20px;
-    }
+            .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n+1){
+                padding-left: 0;
+                margin-left: 0;
+            }
 
-    .pagination .btn{
-        padding: 0 15px 0 0;
-        border: none;
-        color: rgb(142, 142, 142);
+            .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n){
+                padding-right: 0;
+                margin-right: 0;
+            }
 
-    }
+            .IDX-wrapper-standard .IDX-resultsPhoto {
+                text-align: initial;
+            }
+        }
 
-    .pagination-total{
-        color: rgb(142, 142, 142);
-        flex: 1;
-        text-align: right;
-    }
+        @media (max-width: 960px) {
+            .section-0 {
+                padding: 40px 30px;
+                margin-top: -80px;
+                padding-top: 120px;
+            }
 
-    .pagination .btn.active{
-        color: rgb(225, 175, 90);
-    }
+            .IDX-wrapper-standard .IDX-resultsCell {
+                flex: 0 0 100%;
+                max-width: 100%;
+                padding: 0;
+            }
 
-    .pagination .btn-no-hover{
-        cursor: initial !important;
-        transform: scale(1);
-    }
+            .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n+1) {
+                padding-left: 0;
+                margin-left: 0;
+            }
+
+            .IDX-wrapper-standard .IDX-resultsCell:nth-child(2n) {
+                padding-right: 0;
+                margin-right: 0;
+            }
+
+            .IDX-wrapper-standard .IDX-resultsPhoto {
+                text-align: initial;
+            }
+        }
 
 
-</style>
+        /*Pagination*/
+        .container-pagination{
+            display: flex;
+            margin-bottom: 30px;
+        }
 
+        .pagination {
+            display: block;
+        }
 
+        .pagination{
+            min-height: 20px;
+        }
 
+        .pagination .btn{
+            padding: 0 15px 0 0;
+            border: none;
+            color: rgb(142, 142, 142);
+
+        }
+
+        .pagination-total{
+            color: rgb(142, 142, 142);
+            flex: 1;
+            text-align: right;
+        }
+
+        .pagination .btn.active{
+            color: rgb(225, 175, 90);
+        }
+
+        .pagination .btn-no-hover{
+            cursor: initial !important;
+            transform: scale(1);
+        }
+
+        /*subheader*/
+        .sub-header{
+            margin-bottom: 40px;
+        }
+
+        .sub-header-1{
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-end;
+            margin-bottom: 20px;
+        }
+
+        .sub-header-1 > h1, .sub-header-2 > h3{
+            color: rgb(142, 142, 142);
+        }
+
+        .sub-header-1 > h1.active, .sub-header-2 > h3.active{
+            color: rgb(225, 175, 90);
+        }
+
+        .sub-header-2{
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-end;
+        }
+
+    </style>
 
 
 
 <div class="section-0">
 
 
+{{--Esto es lo que va arriba de las casas--}}
+<div class="sub-header">
+    <div class="sub-header-1">
+        <h1 class="active" style="cursor:pointer;">BRICKELL</h1> <h1 style="margin: 0 20px;">/</h1> <h1 style="cursor: pointer">DOWNTOWN MIAMI</h1>
+    </div>
+    <div class="sub-header-2">
+        <h3 class="active" style="cursor:pointer;">SALES</h3> <h3 style="margin: 0 10px;">/</h3> <h3 style="cursor: pointer">RENTALS</h3>
+    </div>
 
-
-
-
-
+</div>
 
 
 
