f48dc9fc1d21e90b807226722da53247a57f92a8
kmilo.denis <kmilo.denis.glez@gmail.com>
2019-12-05 12:28:38 -0500

    - test email downlaod guide

--- resources/views/emails/guides.blade.php
+++ resources/views/emails/guides.blade.php
@@ -1,4 +1,5 @@
 <h3>{{__("app.download_guides")}}</h3>
 @foreach($guides as $guide)
-    <h4><a href="{{ url(App::getLocale()=="es"? $guide->GuideEsPath : $guide->GuideEnPath)}}">{{App::getLocale()=="es"? $guide->text_es : $guide->text_en}} </a></h4>
+    {{--<h4><a href="{{ url(App::getLocale()=="es"? $guide->GuideEsPath : $guide->GuideEnPath)}}">{{App::getLocale()=="es"? $guide->text_es : $guide->text_en}} </a></h4>--}}
+    <h4><a href="{{ asset(App::getLocale()=="es"? $guide->GuideEsPath : $guide->GuideEnPath)}}">{{App::getLocale()=="es"? $guide->text_es : $guide->text_en}} </a></h4>
 @endforeach
